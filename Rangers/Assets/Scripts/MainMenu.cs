8310e9c2642f9d8157fc56736c29f29aa400110e
Luka <lucca.v.rocha@gmail.com>
2022-01-29 00:14:20 -0300

    Menu Animado e configurado

--- Rangers/Assets/Scripts/MainMenu.cs
+++ Rangers/Assets/Scripts/MainMenu.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame(){
+    public void NewGame(){
+
+    }
+    public void ContinueGame(){
         //FindObjectOfType<AudioManager>().StopAllSounds();
         SceneManager.LoadScene("Start",LoadSceneMode.Single);
     }
+    public void Settings(){
 
+    }
+    public void CreditsGame(){
+        
+    }
     public void QuitGame(){
         Debug.Log("Quit!");
         Application.Quit();
