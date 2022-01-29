8d130642da3568fd4202c363a9fd54d69556e478
Luka <lucca.v.rocha@gmail.com>
2022-01-29 01:42:15 -0300

    Menu localized

---
+++ Rangers/Assets/Scripts/Menu/LocaleLoader.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
+
+public class LocaleLoader : MonoBehaviour
+{
+    public void Selectptbr(){
+        //LocalizationSettings.SelectedLocale;
+    }
+}
--- Rangers/Assets/Scripts/Menu/MainMenu.cs
+++ Rangers/Assets/Scripts/Menu/MainMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Localization.Settings;
 using UnityEngine.UI;
 
 
@@ -24,5 +25,11 @@ public class MainMenu : MonoBehaviour
     public void QuitGame(){
         Debug.Log("Quit!");
         Application.Quit();
+    }
+    public void SetLangPtBr(){
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[1];
+    }
+    public void SetLangEn(){
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[0];
     }
 }
