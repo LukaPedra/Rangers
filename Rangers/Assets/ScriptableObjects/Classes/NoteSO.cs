b5700b2d71f0151d476fd85f7271c103fe5cfb7e
rafaelrc7 <rafaelrc7@gmail.com>
2022-01-30 16:29:06 -0300

    Updated note so

--- Rangers/Assets/ScriptableObjects/Classes/NoteSO.cs
+++ Rangers/Assets/ScriptableObjects/Classes/NoteSO.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Note", menuName = "Scriptable Objects/New note")]
 public class NoteSO : ScriptableObject
 {
-	public string person_id;
+	public string note_id;
 	public string text_en;
 	public string text_pt;
 }
