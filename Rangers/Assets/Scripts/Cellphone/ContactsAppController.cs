738315eccd201fca6c9bd556247535536d6027b8
rafaelrc7 <rafaelrc7@gmail.com>
2022-01-29 21:11:30 -0300

    Updated contact app controller

--- Rangers/Assets/Scripts/Cellphone/ContactsAppController.cs
+++ Rangers/Assets/Scripts/Cellphone/ContactsAppController.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class ContactsAppController : MonoBehaviour
 {
 	[SerializeField] private GameObject contactPrefab;
+	[SerializeField] private GameObject contactsPanel;
 
 	private Hashtable contactsTable = new Hashtable();
 
 	public void AddContact(PersonSO person) {
 		GameObject contact = Instantiate(contactPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+		contact.transform.parent = contactsPanel.transform;
 		contact.GetComponent<ContactBehaviour>().ContactID = person.pid;
 		contactsTable.Add(person.pid, contact);
 	}
