0448763bc7976b25cfb87b1f195f93c9f2cb1406
Luka <lucca.v.rocha@gmail.com>
2022-01-30 10:00:26 -0300

    Dialogue start

--- Rangers/Assets/Scripts/Interaction/InteractMenu.cs
+++ Rangers/Assets/Scripts/Interaction/InteractMenu.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class InteractMenu : MonoBehaviour
 {
     private CursorControls controls;
+    public Dialogue[] dialogues;
+    private Dialogue dialogue;
+    [SerializeField] private DialogueManager DialogueManager;
     [SerializeField] private Animator animator;
     private bool isMenuOpen;
     public GameManager gameManager;
+    public Camera cameraMenu;
     private void Awake(){
-        //gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         controls = new CursorControls();
     }
     public void OpenMenu(){
